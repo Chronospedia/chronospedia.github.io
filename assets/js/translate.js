7d6681702a93fe537ee4fd2fc9de96dd889771ff
TitouanBoudart <titouan.boudart@gmail.com>
2023-12-01 17:04:59 +0100

    erreur trad

--- assets/js/translate.js
+++ assets/js/translate.js
@@ -2,7 +2,7 @@
 window.onload = function(){
 
     function googleTranslateElementInit() {
-        new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+        new google.translate.TranslateElement({pageLanguage: 'fr'}, 'google_translate_element');
     }
 
     googleTranslateElementInit();
